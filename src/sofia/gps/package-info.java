dccbf50e930071af0df01cb03eed376f19bc7e4f
Tony Allevato <allevato@vt.edu>
2012-11-01 16:07:43 -0400

    Added package-info.java.

---
+++ src/sofia/gps/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * <p>
+ * Components that interface with the GPS capabilities of an Android device to
+ * provide location detection, location tracking, and proximity alerts.
+ * </p>
+ * 
+ * @since API level 1
+ */
+package sofia.gps;
---
+++ src/sofia/maps/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * <p>
+ * Components that ease the use of the Google Maps capabilities on Android and
+ * provide added features not included in the standard API, such as popup
+ * balloons when markers are clicked, and a route overlay that draws a line
+ * connecting multiple points of interest.
+ * </p>
+ * 
+ * @since API level 1
+ */
+package sofia.maps;
